5e4f77a491b743d1f61621f781aa60d3f28e96f1
973240647 <35675986+973240647@users.noreply.github.com>
2018-01-22 12:45:34 +0800

    Add files via upload

---
+++ CSS/AB.js
@@ -0,0 +1,28 @@
+var a=document.getElementById('_a');
+var dl=document.getElementById('dl');
+var s=document.getElementById('_s');
+var sc=document.getElementById('sc');
+ _a.onmouseover=function funtion_name(){
+    dl.setAttribute("style","width:270px;height:237px;");
+};
+_a.onmouseout=function f(){
+    dl.setAttribute("style","width:0px;height:0px;");
+};
+dl.onmouseover=function fu(){
+    dl.setAttribute("style","width:270px;height:237px;");
+};
+dl.onmouseout=function fune(){
+    dl.setAttribute("style","width:0px;height:0px;");
+};
+ _s.onmouseover=function fe(){
+    sc.setAttribute("style","width:270px;height:70px;");
+};
+_s.onmouseout=function fn(){
+    sc.setAttribute("style","width:0px;height:0px;");
+};
+sc.onmouseover=function fw(){
+    sc.setAttribute("style","width:270px;height:70px;");
+};
+sc.onmouseout=function fv(){
+    sc.setAttribute("style","width:0px;height:0px;");
+};
